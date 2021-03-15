23d758de821e2beb385db1e94ce1bd077f3414e0
bebusl <bebus1998@naver.com>
2021-03-16 01:13:07 +0900

    design side-menu
    1. 사이드 네비게이션 바 디자인
    2. 로그인/로그아웃 시 메뉴 전환 되도록 함
    3. 로그인/회원가입 페이지 링크 연결

--- projects/main/src/app/shared/components/side-menu/side-menu.component.ts
+++ projects/main/src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuList{
+  name:string,
+  link:string
+}
+
+
 @Component({
   selector: 'sw-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+  styleUrls: ['./side-menu.component.scss'],
 })
+
 export class SideMenuComponent implements OnInit {
+  constructor() {}
+  lists: MenuList[];
+  
+  logOut:MenuList[]=[
+    {name:'noneLogin1',link:"#"}, 
+    {name:'로그인페이지',link:"/account/login"},
+    {name:'회원가입페이지',link:"/account/join"}];
+  
+  logIn:MenuList[]=[
+    {name:'Login1',link:"#"},
+    {name:'Login2',link:"#"},
+    {name:'Login3',link:"#"}]
+  
+  loginStatus:Boolean=false;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  setLogin(status:Boolean){
+    this.loginStatus=status;
+    if(status==true) this.lists=this.logIn;
+    else this.lists=this.logOut;
   }
 
+  ngOnInit(): void {
+    this.lists=this.logOut;
+  }
 }
--- projects/main/src/app/shared/shared.module.ts
+++ projects/main/src/app/shared/shared.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent, SideMenuComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [HeaderComponent, SideMenuComponent]
+  imports: [CommonModule, MatSidenavModule, MatButtonModule],
+  exports: [HeaderComponent, SideMenuComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
