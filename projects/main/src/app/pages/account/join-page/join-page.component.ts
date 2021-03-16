569ddcb9de854f3aeed81dd1bf79009d62963490
CheezeLatte <lee50505223@gmail.com>
2021-03-16 19:05:15 +0900

    password-aligin

--- projects/main/src/app/pages/account/join-page/join-page.component.ts
+++ projects/main/src/app/pages/account/join-page/join-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, EventEmitter} from '@angular/core';
-import { FormBuilder} from '@angular/forms';
-interface formObjects {
+import { FormBuilder } from '@angular/forms';
+interface FormObject {
   id: string,
   name: string,
   password: string,
@@ -10,22 +10,24 @@ interface formObjects {
   affiliation: string
 }
 
-interface formClasses{
+interface FormClass{
   class: string,
-  korean: string
+  korean: string,
+  note?: string
 }
 @Component({
   selector: 'sw-join-page',
   templateUrl: './join-page.component.html',
   styleUrls: ['./join-page.component.scss']
 })
+
 export class JoinPageComponent implements OnInit {
   joinForm;
   formSelected = new EventEmitter();
-  formClass:Array<formClasses> = [
-    {class : 'id', korean:'아이디'},
-    {class : 'password', korean:'비밀번호'},
-    {class : 'password', korean:'비밀번호 확인'},
+  formClass:Array<FormClass> = [
+    {class : 'id', korean:'아이디', note:'4~12글자'},
+    {class : 'password', korean:'비밀번호', note:'8글자 이상, 영문과 숫자 조합'},
+    {class : 'password', korean:'비밀번호 확인',},
     {class : 'name', korean:'이름'},
     {class : 'gradeNum', korean:'학번'},
     {class : 'affiliation', korean:'소속'},
@@ -42,15 +44,14 @@ export class JoinPageComponent implements OnInit {
         email: '',
         affiliation: ''
       })
-
-
     }
+
   ngOnInit(): void {
     throw new Error('Method not implemented.');
 
 
   }
-  onSubmit(value:string ){
+  onSubmit(value: FormObject ){
 
   }
 }
