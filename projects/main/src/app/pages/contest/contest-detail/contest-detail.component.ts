7f7148678ede9f8d84dfacbbe41dbdcf26c100e5
DongWooE <ehddn977@naver.com>
2021-03-30 02:10:00 +0900

    Add pdf viewer

--- projects/main/src/app/pages/contest/contest-detail/contest-detail.component.ts
+++ projects/main/src/app/pages/contest/contest-detail/contest-detail.component.ts
@@ -8,12 +8,13 @@ import { ActivatedRoute, ParamMap, Params } from '@angular/router';
 })
 export class ContestDetailComponent implements OnInit {
   id: string
+  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
   constructor(
     private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((params: ParamMap)=> {
+    this.route.paramMap.subscribe((params: ParamMap) => {
       this.id = params.get('id');
     })
   }
--- projects/main/src/app/pages/contest/contest.module.ts
+++ projects/main/src/app/pages/contest/contest.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card'
 import { ContestRoutingModule } from './contest-routing.module';
 import { ContestDetailComponent } from './contest-detail/contest-detail.component';
-
+import { PdfViewerModule } from 'ng2-pdf-viewer'
 
 @NgModule({
   declarations: [ContestDetailComponent],
   imports: [
     CommonModule,
     ContestRoutingModule,
-    MatCardModule
+    MatCardModule,
+    PdfViewerModule
   ]
 })
 export class ContestModule { }
