2cef2cafe8d749cc0adbef1335000361dd160158
DongWooE <ehddn977@naver.com>
2021-03-30 13:23:43 +0900

    Add prev and next button

--- projects/main/src/app/pages/contest/contest-detail/contest-detail.component.ts
+++ projects/main/src/app/pages/contest/contest-detail/contest-detail.component.ts
@@ -8,10 +8,27 @@ import { ActivatedRoute, ParamMap, Params } from '@angular/router';
 })
 export class ContestDetailComponent implements OnInit {
   id: string
-  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+  src = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
   constructor(
     private route: ActivatedRoute,
   ) { }
+
+  page: number = 1;
+  totalPages: number;
+  isLoaded: boolean = false;
+
+  afterLoadComplete(pdfData: any) {
+    this.totalPages = pdfData.numPages;
+    this.isLoaded = true;
+  }
+
+  nextPage() {
+    this.page++;
+  }
+
+  prevPage() {
+    this.page--;
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
