0c00c30edb17a6e28701b47812c244c6ddbe03b5
L-o-g-a-n <xy.logandev@gmail.com>
2021-04-02 01:50:44 +0900

    main-page 속 contest-list 컴포넌트 추가

---
+++ projects/main/src/app/pages/main-page/contest-list/contest-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sw-contest-list',
+  templateUrl: './contest-list.component.html',
+  styleUrls: ['./contest-list.component.scss']
+})
+export class ContestListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/main/src/app/pages/main-page/main-page.module.ts
+++ projects/main/src/app/pages/main-page/main-page.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
+import { ContestListComponent } from './contest-list/contest-list.component';
 
-
+import { MatSliderModule } from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     MainPageComponent,
+    ContestListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   exports: [
     MainPageComponent
