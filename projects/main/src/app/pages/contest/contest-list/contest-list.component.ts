4c02a28209132fb8111f45ba55acae96ccf17254
DongWooE <ehddn977@naver.com>
2021-04-23 02:24:34 +0900

    Add enroll button and Testing token

--- projects/main/src/app/pages/contest/contest-list/contest-list.component.ts
+++ projects/main/src/app/pages/contest/contest-list/contest-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {ContestDetailService, Post} from '../contest-detail.service'
+import { ContestDetailService, Post } from '../contest-detail.service'
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY, StorageService } from '../../../services/storage.service';
 @Component({
   selector: 'sw-contest-list',
   templateUrl: './contest-list.component.html',
   styleUrls: ['./contest-list.component.scss']
 })
 export class ContestListComponent implements OnInit {
-  contests:Array<Post>
-  constructor(detail:ContestDetailService) {
+  contests: Array<Post>
+  constructor(detail: ContestDetailService) {
     this.contests = detail.getContests();
   }
+
+  //enrolling User
+  protected enrollUser = function (_id: any) {
+    const storageService = new StorageService;
+    const refreshToken: string = storageService.get(REFRESH_TOKEN_KEY);
+    const accessToken: string = storageService.get(ACCESS_TOKEN_KEY);
+    console.log(` id :  ${_id}  token : ${refreshToken} ${accessToken}`);
+  }
+
 
   ngOnInit(): void {
   }
