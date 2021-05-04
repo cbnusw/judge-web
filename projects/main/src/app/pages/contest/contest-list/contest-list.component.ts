fd908ed121834f531316b0569b0bd0b1c88bb7f7
DongWooE <ehddn977@naver.com>
2021-05-04 22:11:29 +0900

    Modify judge-web

--- projects/main/src/app/pages/contest/contest-list/contest-list.component.ts
+++ projects/main/src/app/pages/contest/contest-list/contest-list.component.ts
@@ -1,44 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { ContestDetailService, Post } from '../contest-detail.service'
+import { ContestDetailService, Post, PostEnrollData } from '../contest-detail.service'
 import { HttpClient, HttpHeaders, HttpXsrfTokenExtractor } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { Subscription } from 'rxjs';
 import { ACCESS_TOKEN_KEY, StorageService } from '../../../services/storage.service';
-
+import { AuthService } from '../../../services/auth.service';
 @Component({
   selector: 'sw-contest-list',
   templateUrl: './contest-list.component.html',
   styleUrls: ['./contest-list.component.scss']
 })
 export class ContestListComponent implements OnInit {
-  private readonly BASE_URL = environment.authHost;
   subscription: Subscription;
   contests: Array<Post>
-  constructor(private http: HttpClient, detail: ContestDetailService) {
+  constructor(private http: HttpClient, detail: ContestDetailService, private authService: AuthService) {
     this.contests = detail.getContests();
-    this.http = http;
   }
 
   //enrolling User
   protected enrollUser = function (_id: any) {
-    const storageService = new StorageService;
-    const token = storageService.get(ACCESS_TOKEN_KEY);
+    const userId = this.authService.me;
+    const contestId = _id;
 
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `${token}`
-    });
-    let options = { headers: headers };
-    this.subscription = this.http.post('http://localhost:3001/v1/contest-enroll', { id: `${_id}` }, options)
-      .subscribe(
-        res => {
-          console.log(res);
-        }
-      )
+    return this.detail.postEnrollments(userId, contestId);
   }
-
-
-
   ngOnInit(): void {
   }
 
