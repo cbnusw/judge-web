14a2440d150118a0fd435e18fc34dc6e37d42623
kmahn <ahnmo777@gmail.com>
2021-05-28 16:11:55 +0900

    참가 신청한 대회 뜨는 오류 수정

--- projects/main/src/app/pages/main-page/components/my-registered-contest/my-registered-contest.component.ts
+++ projects/main/src/app/pages/main-page/components/my-registered-contest/my-registered-contest.component.ts
@@ -18,6 +18,6 @@ export class MyRegisteredContestComponent extends AbstractSearchDirective<IConte
   }
 
   protected requestObservable(params: IParams | undefined): Observable<IListResponse<IContest>> {
-    return this.contestService.getProgressingContests();
+    return this.contestService.getApplyingContests();
   }
 }
